9212e454b864ab285dced2c03246219e2b330528
Cesar Bernardes <cesar.augusto.bernardes@cern.ch>
2018-07-05 00:09:11 +0200

    Adding new pixel tracking sequence for Phase1 geometry

--- RecoHI/HiTracking/python/HILowPtConformalPixelTracks_cfi.py
+++ RecoHI/HiTracking/python/HILowPtConformalPixelTracks_cfi.py
@@ -41,6 +41,80 @@ hiConformalPixelTracks = cms.EDProducer("PixelTrackProducer",
                                         Cleaner = cms.string("trackCleaner")
                                         )
 
+
+###Pixel Tracking -  PhaseI geometry
+
+#Tracking regions - use PV from pp tracking
+from RecoTracker.TkTrackingRegions.globalTrackingRegionWithVertices_cfi import globalTrackingRegionWithVertices as _globalTrackingRegionWithVertices
+hiConformalPixelTracksPhase1TrackingRegions = _globalTrackingRegionWithVertices.clone(RegionPSet=dict(
+    precise = True,
+    useMultipleScattering = False,
+    useFakeVertices       = False,
+    beamSpot = "offlineBeamSpot",
+    useFixedError = True,
+    nSigmaZ = 3.0,
+    sigmaZVertex = 3.0,
+    fixedError = 0.2,
+    VertexCollection = "goodOfflinePrimaryVertices",
+    ptMin = 0.3,
+    useFoundVertices = True,
+    originRadius = 0.2 
+))
+
+# SEEDING LAYERS
+# Using 4 layers layerlist
+from RecoHI.HiTracking.hiLowPtQuadStep_cff import hiLowPtQuadStepSeedLayers as hiConformalPixelTracksPhase1SeedLayers
+
+# Hit ntuplets
+from RecoHI.HiTracking.hiLowPtQuadStep_cff import hiLowPtQuadStepTracksHitDoubletsCA 
+hiConformalPixelTracksPhase1HitDoubletsCA = hiLowPtQuadStepTracksHitDoubletsCA.clone(
+	seedingLayers = "hiConformalPixelTracksPhase1SeedLayers",
+        trackingRegions = "hiConformalPixelTracksPhase1TrackingRegions"
+)
+
+from RecoHI.HiTracking.hiLowPtQuadStep_cff import hiLowPtQuadStepTracksHitQuadrupletsCA
+hiConformalPixelTracksPhase1HitQuadrupletsCA = hiLowPtQuadStepTracksHitQuadrupletsCA.clone(
+	doublets = "hiConformalPixelTracksPhase1HitDoubletsCA",
+        CAPhiCut = cms.double(0.2),
+        CAThetaCut = cms.double(0.0012),
+        SeedComparitorPSet = cms.PSet(
+           ComponentName = cms.string('none')
+        ),
+        extraHitRPhitolerance = cms.double(0.032),
+        maxChi2 = cms.PSet(
+           enabled = cms.bool(True),
+           pt1 = cms.double(0.7),
+           pt2 = cms.double(2),
+           value1 = cms.double(200),
+           value2 = cms.double(50)
+        ),
+        mightGet = cms.untracked.vstring('IntermediateHitDoublets_hiConformalPixelTracksPhase1HitDoubletsCA__RECO'),
+)
+
+#Filter
+hiConformalPixelTracksPhase1Filter = hiConformalPixelFilter.clone(
+	VertexCollection = cms.InputTag("goodOfflinePrimaryVertices"),
+        chi2 = cms.double(999.9),
+        lipMax = cms.double(999.0),
+        nSigmaLipMaxTolerance = cms.double(999.9),
+        nSigmaTipMaxTolerance = cms.double(999.0),
+        ptMax = cms.double(999999),
+        ptMin = cms.double(0.30),
+        tipMax = cms.double(999.0)
+)
+
+#Pixel tracks
+hiConformalPixelTracksPhase1 = cms.EDProducer("PixelTrackProducer",
+    Cleaner = cms.string('pixelTrackCleanerBySharedHits'),
+    Filter = cms.InputTag("hiConformalPixelTracksPhase1Filter"),
+    Fitter = cms.InputTag("pixelFitterByConformalMappingAndLine"),
+    SeedingHitSets = cms.InputTag("hiConformalPixelTracksPhase1HitQuadrupletsCA"),
+    mightGet = cms.untracked.vstring('RegionsSeedingHitSets_hiConformalPixelTracksPhase1HitQuadrupletsCA__RECO')
+)
+
+
+
+
 hiConformalPixelTracksSequence = cms.Sequence(
     hiTrackingRegionWithVertex +
     hiConformalPixelTracksHitDoublets +
@@ -49,3 +123,17 @@ hiConformalPixelTracksSequence = cms.Sequence(
     hiConformalPixelFilter +
     hiConformalPixelTracks
 )
+
+
+
+hiConformalPixelTracksSequence_Phase1 = cms.Sequence(
+    hiConformalPixelTracksPhase1TrackingRegions +
+    hiConformalPixelTracksPhase1SeedLayers +
+    hiConformalPixelTracksPhase1HitDoubletsCA +
+    hiConformalPixelTracksPhase1HitQuadrupletsCA +
+    pixelFitterByConformalMappingAndLine +
+    hiConformalPixelTracksPhase1Filter +
+    hiConformalPixelTracksPhase1 
+)
+
+
